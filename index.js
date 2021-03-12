b1d69d60cf5b98c77ed8e9e545e1216cc2df8799
Fai <chepngetichfaith46@gmail.com>
2021-03-13 02:03:19 +0300

    Working function to produce the total production per sheds

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-var sheds=["shed A", "shed B", "shed C", "shed D", "shed E"];
-    for (var i = 0; i < sheds.length; i++) 
-        {
 
-            console.l0g(sheds);
-        }
-    
+var sheds=[
+    {
+name:"Shed A",
+cows:5,
+litresPerCow:30  
+  },
 
+  {
+    name:"Shed B",
+    cows:10,
+    litresPerCow:70  
+      },
+
+      {
+        name:"Shed C",
+        cows:10,
+        litresPerCow:50  
+          },
+
+          {
+            name:"Shed D",
+            cows:5,
+            litresPerCow:100  
+              },
+
+];
+
+//production per shed.;
+
+function productionPerShed(){
+    for (var i = 0; i < sheds.length; i++) {
+        var perShed;
+        perShed=sheds[i].cows*sheds[i].litresPerCow;
+        console.log("The total production for " + sheds[i].name + " is " +perShed+ " per day.") ;
+        
+    }
+}
+
+productionPerShed();
 
